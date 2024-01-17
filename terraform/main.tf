44a34e084b496ed3e96264e92f0477dd0c8194b6
manjumh021 <81350837+manjumh021@users.noreply.github.com>
2024-01-17 18:01:07 +0530

    added skip_final_snapshot = true for terraform destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ resource "aws_db_instance" "rds_database" {
   instance_class        = "db.t2.micro"
   username              = "admin"
   password              = "password12345"
-
+  skip_final_snapshot   = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
   parameter_group_name = "default.mysql5.7"
